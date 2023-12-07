61839cb8553a85eeb57fdbdafae7df86f32bc21e
Anup Gautam <anupgautam430@gmail.com>
2023-12-07 14:24:52 +0545

    sql file and activity update added

---
+++ mvc_php_crud.sql
@@ -0,0 +1,448 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2023 at 06:26 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvc_php_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `officer_id` bigint(20) UNSIGNED NOT NULL,
+  `visitor_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('Leave','Appointment','Break') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Cancelled','Deactivated','Completed') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_on` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`id`, `officer_id`, `visitor_id`, `name`, `type`, `status`, `date`, `start_time`, `end_time`, `created_at`, `updated_at`, `added_on`, `last_updated_on`) VALUES
+(1, 1, 1, 'planning', 'Appointment', 'Active', '2023-12-05', '00:33:00', '01:33:00', '2023-12-04 13:03:09', '2023-12-04 13:03:09', '2023-12-04 18:48:09', NULL),
+(2, 1, 1, 'planning and deployment', 'Appointment', 'Active', '2023-12-05', '10:59:00', '13:36:00', '2023-12-04 13:06:52', '2023-12-04 23:24:49', '2023-12-04 18:51:52', NULL),
+(3, 2, 1, 'execution', 'Appointment', 'Active', '2023-12-11', '10:45:00', '11:45:00', '2023-12-06 11:19:54', '2023-12-06 11:19:54', '2023-12-06 17:04:54', NULL),
+(4, 1, 1, 'execution', 'Appointment', 'Active', '2023-12-08', '11:05:00', '12:05:00', '2023-12-06 11:39:04', '2023-12-06 11:39:04', '2023-12-06 17:24:04', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `visitor_id` bigint(20) UNSIGNED NOT NULL,
+  `officer_id` bigint(20) UNSIGNED NOT NULL,
+  `appointment_time` datetime NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `visitor_id`, `officer_id`, `appointment_time`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, '2023-12-06 09:23:00', 'inactive', '2023-12-05 02:54:09', '2023-12-06 12:02:35'),
+(2, 1, 1, '2023-12-07 10:30:00', 'active', '2023-12-05 21:58:54', '2023-12-06 08:45:19'),
+(3, 3, 3, '2023-12-07 01:52:00', 'active', '2023-12-06 02:22:50', '2023-12-06 08:44:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_12_03_075549_create_post_table', 1),
+(6, '2023_12_03_132730_create_visitor_table', 1),
+(7, '2023_12_04_060209_create_offficers_table', 1),
+(8, '2023_12_04_164658_create_workdays_table', 1),
+(10, '2023_12_04_174712_create_activities_table', 2),
+(11, '2023_12_05_075400_create_appointments_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `officers`
+--
+
+CREATE TABLE `officers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `post_id` bigint(20) UNSIGNED NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `work_start_time` time NOT NULL,
+  `work_end_time` time NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `officers`
+--
+
+INSERT INTO `officers` (`id`, `name`, `post_id`, `status`, `work_start_time`, `work_end_time`, `created_at`, `updated_at`) VALUES
+(1, 'Officer Anup', 1, 'Active', '10:00:00', '18:00:00', '2023-12-04 11:15:28', '2023-12-06 21:58:03'),
+(2, 'Aditya', 2, 'Active', '10:00:00', '17:00:00', '2023-12-04 11:15:55', '2023-12-06 22:07:06'),
+(3, 'Prayash', 3, 'Active', '10:00:00', '17:00:00', '2023-12-05 00:30:53', '2023-12-06 21:49:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'CEO', 'active', '2023-12-04 11:14:26', '2023-12-06 01:59:37'),
+(2, 'Manager', 'active', '2023-12-04 11:14:31', '2023-12-06 01:50:21'),
+(3, 'Staff', 'active', '2023-12-04 11:14:37', '2023-12-06 01:50:24'),
+(4, 'Accountant', 'active', '2023-12-06 02:00:04', '2023-12-06 21:04:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
+
+CREATE TABLE `visitors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `Name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Mobile_no` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Email_Address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Status` enum('active','inactive') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `visitors`
+--
+
+INSERT INTO `visitors` (`id`, `Name`, `Mobile_no`, `Email_Address`, `Status`, `created_at`, `updated_at`) VALUES
+(1, 'Anup Gautam', '9812345678', 'anup01@gmail.com', 'active', '2023-12-04 11:15:05', '2023-12-06 08:45:19'),
+(2, 'Prayash', '98624157', 'prayash@gmail.com', 'inactive', '2023-12-05 02:45:07', '2023-12-06 12:02:35'),
+(3, 'Ankit', '9812345644', 'ankit01@gmail.com', 'active', '2023-12-05 02:49:29', '2023-12-06 08:44:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_days`
+--
+
+CREATE TABLE `work_days` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `officer_id` bigint(20) UNSIGNED NOT NULL,
+  `day_of_week` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `work_days`
+--
+
+INSERT INTO `work_days` (`id`, `officer_id`, `day_of_week`, `created_at`, `updated_at`) VALUES
+(1, 1, '6', '2023-12-04 11:16:32', '2023-12-05 01:26:13'),
+(2, 2, '6', '2023-12-04 11:32:56', '2023-12-05 01:27:29'),
+(3, 3, '6', '2023-12-05 01:27:06', '2023-12-05 01:49:01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `activities_officer_id_foreign` (`officer_id`),
+  ADD KEY `activities_visitor_id_foreign` (`visitor_id`);
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `appointments_visitor_id_foreign` (`visitor_id`),
+  ADD KEY `appointments_officer_id_foreign` (`officer_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `officers`
+--
+ALTER TABLE `officers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `officers_post_id_foreign` (`post_id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `work_days`
+--
+ALTER TABLE `work_days`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `work_days_officer_id_foreign` (`officer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `officers`
+--
+ALTER TABLE `officers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitors`
+--
+ALTER TABLE `visitors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `work_days`
+--
+ALTER TABLE `work_days`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activities`
+--
+ALTER TABLE `activities`
+  ADD CONSTRAINT `activities_officer_id_foreign` FOREIGN KEY (`officer_id`) REFERENCES `officers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activities_visitor_id_foreign` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_officer_id_foreign` FOREIGN KEY (`officer_id`) REFERENCES `officers` (`id`),
+  ADD CONSTRAINT `appointments_visitor_id_foreign` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`);
+
+--
+-- Constraints for table `officers`
+--
+ALTER TABLE `officers`
+  ADD CONSTRAINT `officers_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `work_days`
+--
+ALTER TABLE `work_days`
+  ADD CONSTRAINT `work_days_officer_id_foreign` FOREIGN KEY (`officer_id`) REFERENCES `officers` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
